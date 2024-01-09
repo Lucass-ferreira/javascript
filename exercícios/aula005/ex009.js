ca94c7ffb8c67e8cb863c5ddf270aa13cf85fb50
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-09 13:15:20 -0300

    Estruturas Condicionais

---
+++ exercícios/aula005/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') { // Condição composta
+    console.log('Voçê é Brasileiro!')
+} else {
+    console.log('Voçê é Estrangeiro')
+}
