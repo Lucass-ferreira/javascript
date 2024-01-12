54f4c504c5db19575c8bfdb4bb23e60223fe4887
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-12 10:51:35 -0300

    fim do exercício hora do dia

--- exercícios/aula007/JavaScript/JavaScript.js
+++ exercícios/aula007/JavaScript/JavaScript.js
@@ -4,13 +4,16 @@ var hora = horaAgora.getHours()
 var Hr = window.document.getElementById('HoraAgr')
 Hr.innerHTML = `Agora são ${hora} horas.`
 
-if (hora >= 0 && hora < 12){
-    document.getElementById('foto').innerHTML = '<img src="imagens/foto-amanhecer-320.webp" alt="foto-amanhecer">'
+if ( hora >= 6 && hora < 12) {
+    document.getElementById('foto').innerHTML = '<img src="imagens/foto-dia.jpg" alt="foto-dia">'
+    document.body.style.background = '#59adf6'
+} else if (hora >= 12 && hora <= 18) {
+    document.getElementById('foto').innerHTML = '<img src="imagens/foto-tarde.jpg" alt="foto-tarde">'
     document.body.style.background = '#efd459'
-} else if (hora <= 18) {
-    document.getElementById('foto').innerHTML = '<img src="imagens/#" alt="#">'
-    document.body.style.background = '#'
+} else if (hora >= 19 && hora <= 23) {
+    document.getElementById('foto').innerHTML = '<img src="imagens/foto-noite.jpg" alt="foto-noite">'
+    document.body.style.background = '#020219'
 } else {
-    document.getElementById('foto').innerHTML = '<img src="imagens/#" alt="#">'
-    document.body.style.background = '#'
+    document.getElementById('foto').innerHTML = '<img src="imagens/foto-amanhecer.webp" alt="foto-amanhecer">'
+    document.body.style.background = '#0d485c'
 }
