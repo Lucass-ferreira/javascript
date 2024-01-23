50dde050957065f3d72a82b658eacdb72a87c950
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-23 11:20:45 -0300

    Estrutura de repetição - No Inicio e  no final

---
+++ exercícios/aula007/reptiçao.js
@@ -0,0 +1,24 @@
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while ( cont <= 10)
+
+
+/*
+var cont = 1
+while ( cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+*/
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
