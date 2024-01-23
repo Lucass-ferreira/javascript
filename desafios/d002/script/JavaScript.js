2435b8f8efc0cd7729eb5782ab02750354c0e445
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-23 10:46:55 -0300

    Ex - Verificador de idade

--- desafios/d002/script/JavaScript.js
+++ desafios/d002/script/JavaScript.js
@@ -1,16 +1,64 @@
-var nasc = window.document.getElementById('inasc')
-var resp = window.document.getElementById('res')
+function verificar() {
+    var data = new Date()
+    var ano_atual = data.getFullYear()
+    var texto_ano = document.getElementById('inasc')
+    var res = document.querySelector('div#res')
 
-var sexo1 = window.document.getElementById('msexo')
-var sexo2 = window.document.getElementById('fsexo')
+    if (texto_ano.value.length == 0 || Number(texto_ano.value) > ano_atual) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var idade = ano_atual - Number(texto_ano.value)
+        var isexo = document.getElementsByName('sexo')
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id' , 'foto')
+        if (isexo[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 5) {
+                //bebe
+                img.setAttribute('src' , 'imagens/m-bebe.jpg')
+            } else if (idade < 12) {
+                //criança
+                img.setAttribute('src' , 'imagens/m-criança.jpg') 
+            } else if ( idade < 18) {
+                //adolescente
+                img.setAttribute('src' , 'imagens/m-adolescente.jpg') 
+            } else if ( idade < 30) {
+                //adulto
+                img.setAttribute('src' , 'imagens/m-adulto.jpg') 
+            } else if (idade < 60) {
+                //meia-idade
+                img.setAttribute('src' , 'imagens/m-meia.jpg') 
+            } else {
+                //idoso
+                img.setAttribute('src' , 'imagens/m-idoso.jpg') 
+            }
+        } else if (isexo[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 5) {
+                //bebe
+                img.setAttribute('src' , 'imagens/f-bebe.jpg')
+            } else if (idade < 12) {
+                //criança
+                img.setAttribute('src' , 'imagens/f-criança.jpg') 
+            } else if ( idade < 18) {
+                //adolescente
+                img.setAttribute('src' , 'imagens/f-adolescente.jpg') 
+            } else if ( idade < 30) {
+                //adulto
+                img.setAttribute('src' , 'imagens/f-adulto.jpg') 
+            } else if (idade < 60) {
+                //meia-idade
+                img.setAttribute('src' , 'imagens/f-meia.jpg') 
+            } else {
+                //idoso
+                img.setAttribute('src' , 'imagens/f-idoso.jpg') 
+            }
+        }
+    }
 
-if( sexo1 == 'on'){
-    var sex = 'Homem'
-} else if (sexo2 == 'on') {
-    var sex = 'Mulher'
-}
-
-function clicar() {
-    var nascim = Number(nasc.value)
-    resp.innerHTML = `${nascim} ${sex} ${res1}`
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
+    
 }
