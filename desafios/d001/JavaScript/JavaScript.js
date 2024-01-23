abf3fef14ef16336c697107cfb55b431637ad8b0
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-23 09:00:37 -0300

    revisão do Ex - Hora do dia

--- desafios/d001/JavaScript/JavaScript.js
+++ desafios/d001/JavaScript/JavaScript.js
@@ -1,19 +1,25 @@
-var horaAgora = new Date()
-var hora = horaAgora.getHours()
+function carregar() {
+    var Hr = window.document.getElementById('HoraAgr')
 
-var Hr = window.document.getElementById('HoraAgr')
-Hr.innerHTML = `Agora são ${hora} horas.`
+    var horaAgora = new Date()
+    var hora = horaAgora.getHours()
+    Hr.innerHTML = `Agora são ${hora} horas.`
 
-if ( hora >= 6 && hora < 12) {
-    document.getElementById('foto').innerHTML = '<img src="imagens/foto-dia.jpg" alt="foto-dia">'
-    document.body.style.background = '#59adf6'
-} else if (hora >= 12 && hora <= 18) {
-    document.getElementById('foto').innerHTML = '<img src="imagens/foto-tarde.jpg" alt="foto-tarde">'
-    document.body.style.background = '#efd459'
-} else if (hora >= 19 && hora <= 23) {
-    document.getElementById('foto').innerHTML = '<img src="imagens/foto-noite.jpg" alt="foto-noite">'
-    document.body.style.background = '#020219'
-} else {
-    document.getElementById('foto').innerHTML = '<img src="imagens/foto-amanhecer.webp" alt="foto-amanhecer">'
-    document.body.style.background = '#0d485c'
+    var foto = window.document.getElementById('imagem')
+
+    
+    if ( hora >= 6 && hora < 12) {
+        foto.src= 'imagens/foto-dia.jpg'
+        document.body.style.background = '#59adf6'
+    } else if (hora >= 12 && hora <= 18) {
+        foto.src = 'imagens/foto-tarde.jpg'
+        document.body.style.background = '#efd459'
+    } else if (hora >= 19 && hora <= 23) {
+        foto.src = 'imagens/foto-noite.jpg'
+        document.body.style.background = '#020219'
+    } else {
+        foto.src = 'imagens/foto-amanhecer.webp'
+        document.body.style.background = '#0d485c'
+    }
 }
+
