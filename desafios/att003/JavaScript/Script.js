2999a313819ed0c0a8d18dcf18f302ee40eb7c8c
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-25 11:09:50 -0300

    atividade 003

--- desafios/att003/JavaScript/Script.js
+++ desafios/att003/JavaScript/Script.js
@@ -1,1 +1,25 @@
+function clicar() {
+    var numero1 = window.document.querySelector('input#inumero1')
+    var numero2 = window.document.querySelector('input#inumero2')
+    var passo = window.document.querySelector('input#ipasso')
+    var inicio = Number(numero1.value)
+    var fim = Number(numero2.value)
+    var pas = Number(passo.value)
+    var resp = window.document.querySelector('div#resp')
 
+    if( inicio == 0 || fim == 0 || passo == 0 ) {
+        window.alert('[ERRO] verifique os dados e tente novamnete!')
+    } else if (inicio < fim) {
+        var cont = inicio
+        while (cont <= fim) {
+            resp.innerHTML = `${cont}`
+            cont++
+        }
+    } else {
+        var cont = inicio
+        while (cont >= fim) {
+            resp.innerHTML = `${cont}`
+            cont = cont - pas
+        }
+    }
+}
