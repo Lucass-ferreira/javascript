d52a74efb79d361cd739cb0b2b9db59ca16f831b
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-29 10:40:11 -0300

    atualizando o script da att contador

--- desafios/att003/JavaScript/Script.js
+++ desafios/att003/JavaScript/Script.js
@@ -2,24 +2,32 @@ function clicar() {
     var numero1 = window.document.querySelector('input#inumero1')
     var numero2 = window.document.querySelector('input#inumero2')
     var passo = window.document.querySelector('input#ipasso')
-    var inicio = Number(numero1.value)
-    var fim = Number(numero2.value)
-    var pas = Number(passo.value)
     var resp = window.document.querySelector('div#resp')
+
 
     if( numero1.value.length == 0 || numero2.value.length == 0 || passo.value.length == 0 ) {
         window.alert('[ERRO] verifique os dados e tente novamnete!')
-    } else if (inicio < fim) {
-        resp.innerHTML = 'Contando: '
-        for (var cont = inicio; cont <= fim; cont += pas) {
-            resp.innerHTML += ` ${cont} \u{1F449}`
-        }
-        resp.innerHTML += `\u{1F3C1}`
     } else {
         resp.innerHTML = 'Contando: '
-        for (var cont = inicio; cont >= fim; cont -= pas) {
-            resp.innerHTML += ` ${cont} \u{1F449}`
+        var inicio = Number(numero1.value)
+        var fim = Number(numero2.value)
+        var pas = Number(passo.value)
+
+        if (pas <= 0){
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            pas = 1
         }
-        resp.innerHTML += `\u{1F3C1}`
+
+        if (inicio < fim) {
+            for (var cont = inicio; cont <= fim; cont += pas) {
+                resp.innerHTML += ` ${cont} \u{1F449}`
+            }
+            resp.innerHTML += `\u{1F3C1}`
+        } else {
+            for (var cont = inicio; cont >= fim; cont -= pas) {
+                resp.innerHTML += ` ${cont} \u{1F449}`
+            }
+            resp.innerHTML += `\u{1F3C1}`
+        }
     }
 }
