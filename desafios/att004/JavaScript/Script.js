2b50e9b2970d5849d5a2e629ed0e7445a85a691a
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-01-29 13:29:22 -0300

    criando a atividade tabuada

--- desafios/att004/JavaScript/Script.js
+++ desafios/att004/JavaScript/Script.js
@@ -1,1 +1,20 @@
+function clicar() {
+    var numero = window.document.querySelector('input#inumero')
+    var resp = window.document.querySelector('select#itabuada')
+    var num = Number(numero.value)
+
+    if (numero.value.length == 0) {
+        window.alert('[ERRO] Por favor digite um número')
+    } else {
+        var cont = 1
+        resp.innerHTML = ''
+        while ( cont <= 10) {
+        var item = document.createElement('option')
+        item.text = `${num} x ${cont} = ${num*cont}`
+        item.value = `resp${cont}`
+        resp.appendChild(item)
+        cont++
+        }
+    }
+}
 
